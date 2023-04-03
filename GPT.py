dceaa3668f9f8bc949736335712f1d1c76e6aa30
MaximkaT <goplop307@gmail.com>
2023-04-03 23:21:39 +0700

    Explaining the GPT func

--- GPT.py
+++ GPT.py
@@ -2,6 +2,12 @@ import openai
 
 
 def gptReply(messages, user, message):
+    """
+    :param messages: A dictionary with previous messages and roles of those who have written them
+    :param user: User's discord name
+    :param message: Prompt that requires a reply
+    :return: A reply for the prompt based on previous messages
+    """
     try:
         messages[user] += [{'role': 'user', 'content': message}]
     except KeyError:
